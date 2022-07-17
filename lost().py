4375ba18d0101f98727e5e88cd5a76fad41908be
hds <50664844+Amyhds@users.noreply.github.com>
2022-07-17 20:33:04 +0900

    Delete lost().py

--- lost().py
+++
@@ -1,9 +0,0 @@
-expression = input().split('-') # split으로 -와 +기준으로 나눈다
-# ['55', '50+40']
-result = 0
-for i in expression[0].split('+'):
-    result += int(i)
-for i in expression[1:]:
-    for j in i.split('+'):
-        result -= int(j)
-print(result)
