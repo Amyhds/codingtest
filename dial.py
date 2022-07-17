00ecdce04ab8c9526993235ad01d8a8c430362e7
hds <50664844+Amyhds@users.noreply.github.com>
2022-07-17 20:23:39 +0900

    Delete dial.py

--- dial.py
+++
@@ -1,8 +0,0 @@
-alpha = ['ABC','DEF','GHI','JKL','MNO','PQRS','TUV','WXYZ']
-expr = input()
-least = []
-for i in range(len(expr)):
-    for j in range(len(alpha)):
-        if expr[i] in alpha[j]:
-            least.append(j+3)
-print(sum(least))
