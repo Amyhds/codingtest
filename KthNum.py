807a87a6b261e843bf9153192128cbee5369a123
hds <50664844+Amyhds@users.noreply.github.com>
2022-07-17 20:15:13 +0900

    Delete KthNum.py

--- KthNum.py
+++
@@ -1,7 +0,0 @@
-def solution(array, commands):
-    answer = []
-    for i, j, k in commands:
-        array1 = array[i-1:j]
-        array1.sort()
-        answer.append(array1[k-1])
-    return answer
