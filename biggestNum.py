781969e14f62ae789a47fc898f961d8952af0430
hds <50664844+Amyhds@users.noreply.github.com>
2022-07-17 20:15:27 +0900

    Delete biggestNum.py

--- biggestNum.py
+++
@@ -1,7 +0,0 @@
-def solution(numbers):
-    if sum(numbers) == 0:
-        return '0'
-    numbers = list(map(str, numbers))
-    numbers.sort(key=lambda x: x * 3, reverse=True)
-
-    return ''.join(numbers)
