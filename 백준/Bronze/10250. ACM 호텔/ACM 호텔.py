ea874cbb6e8ec91ffbb54b38cf50b12d2d0bee1c
hds <50664844+Amyhds@users.noreply.github.com>
2022-07-19 21:07:12 +0900

    [Bronze III] Title: ACM 호텔, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -1,11 +1,9 @@
 T = int(input())
 for _ in range(T):
     H, W, N = map(int, input().split())
-    start = 1
-    for _ in range(N):
-        start += 100
-        if start//100 == H+1:
-            start = start - H*100 + 1
-    print(start)
-            
-            
+    floor = N % H
+    room = N // H + 1
+    if floor == 0:
+        floor = H
+        room -= 1
+    print(floor*100 + room)
