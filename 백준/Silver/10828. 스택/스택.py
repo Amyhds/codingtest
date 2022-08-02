7b60fbcec2660fdc28f1bccc4128b4b68a28f7a9
hds <50664844+Amyhds@users.noreply.github.com>
2022-08-02 13:30:30 +0900

    [Silver IV] Title: 스택, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+stk = []
+for _ in range(N):
+    cmd = input().split()
+    if cmd[0] == 'push':
+        stk.append(cmd[1])
+    elif cmd[0] == 'pop':
+        if stk:
+            print(stk.pop())
+        else:
+            print(-1)
+    elif cmd[0] == 'size':
+        print(len(stk))
+    elif cmd[0] == 'empty':   
+        if stk:
+            print(0)
+        else:
+            print(1)
+    elif cmd[0] == 'top':
+        if stk:
+            print(stk[-1])
+        else:
+            print(-1)
