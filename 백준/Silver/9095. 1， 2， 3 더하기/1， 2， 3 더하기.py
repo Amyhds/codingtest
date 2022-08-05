630767ba5746c03fe7a894c5f6a32e6adbbb8642
hds <50664844+Amyhds@users.noreply.github.com>
2022-08-05 12:58:16 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -0,0 +1,16 @@
+def ott(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    elif n == 3:
+        return 4
+    else:
+        return ott(n-1) + ott(n-2) + ott(n-3)
+    
+T = int(input())
+for _ in range(T):
+    n = int(input())
+    print(ott(n))
+    
+    
