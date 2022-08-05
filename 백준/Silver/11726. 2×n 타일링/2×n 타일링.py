b1d2bc493a0f38f1fc20b94e2d84a7903fc816c2
hds <50664844+Amyhds@users.noreply.github.com>
2022-08-05 13:33:10 +0900

    [Silver III] Title: 2×n 타일링, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -1,10 +1,5 @@
 n = int(input())
-d = [0] * (n+1)
-for i in range(1, n+1):
-    if i == 1:
-        d[i] = 1
-    elif i == 2:
-        d[i] = 2
-    else:
-        d[i] = d[i-1] + d[i-2]
+d = [0, 1, 2]
+for i in range(3, 1001):
+    d.append(d[i-1] + d[i-2])
 print(d[n]%10007)                
