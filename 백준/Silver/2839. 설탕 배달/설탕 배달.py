6c8aece58cf22c35def2e45104282941f8094f01
hds <50664844+Amyhds@users.noreply.github.com>
2022-08-11 13:06:23 +0900

    [Silver IV] Title: 설탕 배달, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,4 +1,4 @@
-N = int(input())
+'''N = int(input())
 d = [5001] * (N+1)
 d[0] = 0
 money = [3, 5]
@@ -10,4 +10,17 @@ for i in range(2):
 if d[N] == 5001:
     print(-1)
 else:
-    print(d[N])
+    print(d[N])'''
+    
+N = int(input())
+res = 0
+while N >= 0:
+    if N % 5 == 0:
+        res += N // 5
+        print(res)
+        break
+    N -= 3
+    res += 1
+else:
+    print(-1)
+    
