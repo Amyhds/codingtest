deebfc62529c1f1d77f33df15cb133d128a12568
hds <50664844+Amyhds@users.noreply.github.com>
2022-08-12 20:25:31 +0900

    [Silver III] Title: 스택 수열, Time: 4168 ms, Memory: 32424 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,21 @@
+n = int(input())
+seq = []
+res = []
+start = 1
+flag = True
+for _ in range(n):
+    num = int(input())
+    while start <= num:
+        seq.append(start)
+        res.append('+')
+        start += 1
+    if seq[-1] == num:
+        seq.pop()
+        res.append('-')
+    else:
+        flag = False
+if flag == False:
+    print('NO')
+else:
+    for i in res:
+        print(i)
