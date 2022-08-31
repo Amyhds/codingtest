cba34ebb9ee6545517cb499c8137d86fac771905
hds <50664844+Amyhds@users.noreply.github.com>
2022-08-31 11:38:34 +0900

    [Silver II] Title: 나무 자르기, Time: 516 ms, Memory: 260064 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,17 +1,17 @@
 import sys
 input = sys.stdin.readline
 N, M = map(int, input().split())
-trees = list(map(int, input().split()))
-start = 1
-end = max(trees)
-while start <= end:
-    mid = (start + end) // 2
-    res = 0
-    for i in range(N):
-        if trees[i] > mid:
-            res += trees[i] - mid 
-    if res < M:
-        end = mid - 1
+tree = list(map(int, input().split()))
+short = 0
+long = max(tree)
+while short <= long:
+    mid = (short + long) // 2
+    Sum = 0
+    for t in tree:
+        if t > mid:
+            Sum += (t - mid)
+    if Sum >= M:
+        short = mid + 1
     else:
-        start = mid + 1
-print(end)
+        long = mid - 1
+print(long)
