aba7445329948a302d579b6b01e414df348e6865
hds <50664844+Amyhds@users.noreply.github.com>
2022-09-01 12:23:51 +0900

    [Silver III] Title: Four Squares, Time: 136 ms, Memory: 114720 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.py
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -1,4 +1,5 @@
 #아 너무 어렵다 무슨소리지
+#https://pgh268400.tistory.com/464
 import sys
 input = sys.stdin.readline
 n = int(input())
@@ -13,3 +14,18 @@ for i in range(2, n+1):
         k += 1
     d[i] = count + 1
 print(d[n])
+'''
+n = int(input())
+dp = [4] * (n+1)
+dp[1] = 1
+
+for i in range(2, n+1):
+    if int(i ** 0.5) ** 2 == i:
+        dp[i] = 1
+    else:
+        j = 1
+        while j**2 < i:
+            dp[i] = min(dp[j**2] + dp[i-j**2], dp[i])
+            j += 1
+print(dp[n])
+'''         
