a63f6d9367ae89dde0d6354cee651818c2f1fba7
hds <50664844+Amyhds@users.noreply.github.com>
2022-09-14 11:16:58 +0900

    [D2] Title: 수도 요금 경쟁 D2 Attack, Time: 153 ms, Memory: 58,240 KB -BaekjoonHub

---
+++ SWEA/D2/1284. 수도 요금 경쟁 D2 Attack/수도 요금 경쟁 D2 Attack.py
@@ -0,0 +1,12 @@
+T = int(input())
+for test_case in range(1, T + 1):
+    P, Q, R, S, W = map(int, input().split())
+    A = P * W
+    if R > W:
+        B = Q
+    else:
+        B = Q + (W-R)*S
+    if A > B:
+        print(f'#{test_case} {B}')
+    else:
+        print(f'#{test_case} {A}')
