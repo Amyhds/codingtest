700c9767c1ce60cd0f7c8e426f7917128e56d2a4
hds <50664844+Amyhds@users.noreply.github.com>
2022-09-30 20:45:38 +0900

    [Silver I] Title: 경로 찾기, Time: 296 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -0,0 +1,9 @@
+N = int(input())
+graph = [list(map(int, input().split())) for _ in range(N)]
+for k in range(N):
+    for i in range(N):
+        for j in range(N):
+            if graph[i][k] and graph[k][j]:
+                graph[i][j] = 1
+for v in graph:
+    print(*v)
