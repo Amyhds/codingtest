b86ba83ab168759cf2713e12b70716073cd0d747
hds <50664844+Amyhds@users.noreply.github.com>
2022-10-06 16:24:57 +0900

    [D4] Title: [S/W 문제해결 기본] 6일차 - 계산기3, Time: 141 ms, Memory: 58,568 KB -BaekjoonHub

---
+++ SWEA/D4/1224. ［S／W 문제해결 기본］ 6일차 － 계산기3/［S／W 문제해결 기본］ 6일차 － 계산기3.py
@@ -0,0 +1,45 @@
+def Postfix(infix):
+    postfix = ''
+    stack = []
+    for i in infix:
+        if i.isdigit():
+            postfix += i
+        else:
+            if i == '(':
+                stack.append(i)
+            elif i == '*':
+                while stack and stack[-1] == '*':
+                    postfix += stack.pop()
+                stack.append(i)
+            elif i == '+':
+                while stack and stack[-1] != '(':
+                    postfix += stack.pop()
+                stack.append(i)
+            elif i == ')':
+                while stack and stack[-1] != '(':
+                    postfix += stack.pop()
+                stack.pop()
+    while stack:
+        postfix += stack.pop()
+    return postfix
+         
+def cal(postfix):
+    stack = []
+    for p in postfix:
+        if p.isdigit():
+            stack.append(int(p))
+        else:
+            second = stack.pop()
+            first = stack.pop()
+            if p == '+':
+                stack.append(first + second)
+            elif p == '*':
+                stack.append(first * second)
+    return stack.pop()
+     
+for tc in range(1, 11):
+    N = int(input())
+    infix = input()
+    postfix = Postfix(infix)
+    res = cal(postfix)
+    print(f'#{tc} {res}')
