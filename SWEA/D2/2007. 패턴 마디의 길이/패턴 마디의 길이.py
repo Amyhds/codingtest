3c45a348159586ed0328a8e34aeca095347ff58e
hds <50664844+Amyhds@users.noreply.github.com>
2022-10-18 20:23:17 +0900

    [D2] Title: 패턴 마디의 길이, Time: 137 ms, Memory: 58,388 KB -BaekjoonHub

--- SWEA/D2/2007. 패턴 마디의 길이/패턴 마디의 길이.py
+++ SWEA/D2/2007. 패턴 마디의 길이/패턴 마디의 길이.py
@@ -1,14 +1,12 @@
-Test = int(input())
-for t in range(1, Test+1):
+T = int(input())
+for tc in range(1, T+1):
     string = input()
-    word = ""
     length = 0
     for i in range(1, 11):
-        word = string[:i]
-        next_word = string[i:i*2]
-        nnext_word = string[i*2:i*3]
-        if word == next_word:
+        w1 = string[:i]
+        w2 = string[i:i*2]
+        w3 = string[i*2:i*3]
+        if w1 == w2 and w2 == w3:
             length = i
             break
-    print("#"+str(t)+" "+str(length))
-    
+    print(f'#{tc} {length}')        
