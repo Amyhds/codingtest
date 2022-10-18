0bac10c32765b64f0863775e45f23699170a1bdc
hds <50664844+Amyhds@users.noreply.github.com>
2022-10-18 21:33:35 +0900

    [D2] Title: 백만 장자 프로젝트, Time: 1,095 ms, Memory: 241,000 KB -BaekjoonHub

--- SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
+++ SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
@@ -1,13 +1,15 @@
 T = int(input())
-for t in range(1, T + 1):
+for tc in range(1, T+1):
     N = int(input())
-    arr = list(map(int, input().split()))
-    res = 0
-    curr = arr[-1]
-    for i in range(N-2, -1, -1):
-        if curr > arr[i]:
-            res += curr - arr[i]
+    price = list(map(int, input().split()))
+    m = 0
+    total = 0
+    for i in range(N-1, -1, -1):
+        if m < price[i]:
+            m = price[i]
         else:
-            curr = arr[i]
-    print("#"+str(t)+" "+str(res))
+            total += m
+            total -= price[i]
+    print(f'#{tc} {total}')
+        
         
