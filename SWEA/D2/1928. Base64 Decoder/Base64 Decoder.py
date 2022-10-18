e216da3aede546b57b02c79d610eb9de6ac10766
hds <50664844+Amyhds@users.noreply.github.com>
2022-10-18 13:45:57 +0900

    [D2] Title: Base64 Decoder, Time: 99 ms, Memory: 43,916 KB -BaekjoonHub

--- SWEA/D2/1928. Base64 Decoder/Base64 Decoder.py
+++ SWEA/D2/1928. Base64 Decoder/Base64 Decoder.py
@@ -1,19 +1,18 @@
 decode = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
           'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9','+','/']
 T = int(input())
-for test_case in range(1, T + 1):
-    word = input()
-    bin_res = ''
+for tc in range(1, T + 1):
+    string = input()
+    res = ''
     ans = ''
-    for i in range(len(word)):
-        num = decode.index(word[i])
-        bin_num = str(bin(num)[2:])
+    for i in range(len(string)):
+        temp = decode.index(string[i])
+        bin_num = str(bin(temp)[2:])
         while len(bin_num) < 6:
             bin_num = '0' + bin_num
-        bin_res += bin_num
-    for j in range(0, len(bin_res), 8):
-        dec = int(bin_res[j:j+8] , 2)
-        ans += chr(dec)
-    print('#'+str(test_case)+' '+ans)
+        res += bin_num
+    for i in range(0, len(res), 8):
+        temp = int(res[i:i+8] , 2)
+        ans += chr(temp)
+    print(f'#{tc} {ans}')    
         
-    
