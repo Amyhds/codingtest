e9c1a0f8c805bd5f90196dc48f7f17568a18a175
hds <50664844+Amyhds@users.noreply.github.com>
2022-10-18 16:32:49 +0900

    [D2] Title: 두 개의 숫자열, Time: 132 ms, Memory: 58,360 KB -BaekjoonHub

--- SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.py
+++ SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.py
@@ -1,29 +1,21 @@
 T = int(input())
-# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-for test_case in range(1, T + 1):
+for tc in range(1, T+1):
     N, M = map(int, input().split())
-    A = list(map(int, input().split()))
-    B = list(map(int, input().split()))
-    ans = 0
-    if len(A) < len(B):
-        s = A
-        l = B
-    elif len(B) < len(A):
-        s = B
-        l = A
+    res = []
+    if N > M:
+        b = N
+        s = M
+        big = list(map(int, input().split()))
+        sml = list(map(int, input().split()))
     else:
-    	for i in range(N):
-            ans += A[i] * B[i]
-            print('#'+str(test_case)+' '+str(ans))
-            break
-    for k in range(abs(N-M)+1):
+        b = M
+        s = N
+        sml = list(map(int, input().split()))
+        big = list(map(int, input().split()))
+    for i in range(b-s+1):
         temp = 0
-        for i in range(len(s)):
-            temp += s[i] * l[i + k]
-        if temp > ans:
-            ans = temp
-    print('#'+str(test_case)+' '+str(ans))
-     
-                
-                
+        for j in range(s):
+            temp += sml[j] * big[i+j]
+        res.append(temp)
+    print(f'#{tc} {max(res)}')
         
