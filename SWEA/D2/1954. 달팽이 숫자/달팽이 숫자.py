d84c9f3ef973ad8ae0d46be4e63ac10581ee019f
hds <50664844+Amyhds@users.noreply.github.com>
2022-10-19 00:40:33 +0900

    [D2] Title: 달팽이 숫자, Time: 133 ms, Memory: 58,380 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
@@ -1,24 +1,23 @@
 T = int(input())
-for test_case in range(1, T + 1):
+for tc in range(1, T+1):
     N = int(input())
-    snail = [[0 for _ in range(N)] for _ in range(N)]
-    dx = [0, 1, 0, -1]
-    dy = [1, 0, -1, 0]
+    d = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    arr = [[0]*N for _ in range(N)]
     x = 0
     y = -1
-    cnt = 1
+    n = 1
     k = 0
-    while cnt <= N*N:
-        nx = x + dx[k]
-        ny = y + dy[k]
-        if 0 <= nx < N and 0 <= ny < N and snail[nx][ny] == 0:
-            snail[nx][ny] = cnt
-            cnt += 1
+    while n <= N*N:
+        nx = x + d[k][0]
+        ny = y + d[k][1]
+        if 0 <= nx < N and 0 <= ny < N and arr[nx][ny] == 0:
+            arr[nx][ny] = n
+            n += 1
             x = nx
             y = ny
         else:
-            k = (k+1)%4
-
-    print('#' + str(test_case))
+            k = (k+1) % 4
+    print(f'#{tc}')
     for i in range(N):
-        print(*snail[i])
+        print(*arr[i])
+        
