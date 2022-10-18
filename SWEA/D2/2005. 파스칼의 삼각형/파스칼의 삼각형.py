d4ff348016cadbc8e41906fde658aab10dc0b91c
hds <50664844+Amyhds@users.noreply.github.com>
2022-10-18 23:40:08 +0900

    [D2] Title: 파스칼의 삼각형, Time: 132 ms, Memory: 58,364 KB -BaekjoonHub

--- SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.py
+++ SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.py
@@ -1,23 +1,20 @@
 T = int(input())
-for test_case in range(1, T + 1):
+for tc in range(1, T+1):
     N = int(input())
-    tri = []
+    pascal = [[] for _ in range(N)]
     for i in range(N):
         if i == 0:
-            tri.append([1])
-        if i == 1:
-            tri.append([1, 1])
-        if i > 1:
-            a = []
-            a.append(1)
+            pascal[i].append(1)
+        elif i == 1:
+            pascal[i].append(1)
+            pascal[i].append(1)
+        else:
+            pascal[i].append(1)
             for j in range(i-1):
-                a.append(tri[i-1][j]+tri[i-1][j+1])
-            a.append(1)
-            tri.append(a)
-    print("#"+str(test_case))
-    for i in tri:
-        for j in i:
-        	print(j, end = ' ')
-        print()
+                pascal[i].append(pascal[i-1][j]+pascal[i-1][j+1])
+            pascal[i].append(1)
+    print(f'#{tc}')
+    for i in range(N):
+        print(*pascal[i])
             
             
