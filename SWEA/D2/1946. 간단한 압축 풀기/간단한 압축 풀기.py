46d4f54bd96856e11d874cc37cb8d43b23b64c6e
hds <50664844+Amyhds@users.noreply.github.com>
2022-10-18 21:58:59 +0900

    [D2] Title: 간단한 압축 풀기, Time: 144 ms, Memory: 58,740 KB -BaekjoonHub

--- SWEA/D2/1946. 간단한 압축 풀기/간단한 압축 풀기.py
+++ SWEA/D2/1946. 간단한 압축 풀기/간단한 압축 풀기.py
@@ -1,22 +1,16 @@
 T = int(input())
-for test_case in range(1, T + 1):
+for tc in range(1, T+1):
     N = int(input())
-    doc = ''
+    res = ''
     for _ in range(N):
         C, K = input().split()
         for _ in range(int(K)):
-            doc += C
-    print('#'+str(test_case))
-    if len(doc) <= 10:
-        print(doc)
-    else:
-        start = 0
-        end = 10
-        while True:
-            print(doc[start:end], end='\n')
-            if len(doc[end:]) < 10:
-                print(doc[end:])
-                break
-            else:
-                start = end
-                end += 10
+            res += C
+    print(f'#{tc}')
+    for i in range(len(res)):
+        if i != 0 and i % 10 == 0:
+            print()
+            print(res[i], end='')
+        else:
+            print(res[i], end='')
+    print()       
