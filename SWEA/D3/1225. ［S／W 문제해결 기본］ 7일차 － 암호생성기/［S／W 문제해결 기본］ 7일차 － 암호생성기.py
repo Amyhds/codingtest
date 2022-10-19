9757fb3da6666a98e1c6cb2fb6105438ff9346c2
hds <50664844+Amyhds@users.noreply.github.com>
2022-10-19 19:50:58 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 186 ms, Memory: 62,884 KB -BaekjoonHub

--- SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.py
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.py
@@ -1,25 +1,24 @@
 from collections import deque
-T = 10
-for test_case in range(1, T + 1):
-    t = int(input())
-    num = list(map(int, input().split()))
-    num = deque(num)
-    cycle = 0
-    minus = 1
+for _ in range(10):
+    tc = int(input())
+    code = list(map(int, input().split()))
+    code = deque(code)
+    out = False
     while True:
-        if cycle == 5:
-            minus = 1
-            cycle = 0
-        first = num.popleft()
-        num.append(first - minus)
-        if first - minus <= 0:
-            last = num.pop()
-            last = 0
-            num.append(last)
+        minus = 1
+        for _ in range(5):
+            start = code.popleft()
+            temp = start - minus
+            if temp <= 0:
+                temp = 0
+                code.append(temp)
+                out = True
+                break
+            else:
+                code.append(temp)
+                minus += 1
+        if out:
             break
-        minus += 1
-        cycle += 1
-    print('#'+str(t), end = ' ')
-    for n in num:
-        print(n, end = ' ')
-    print()
+            
+    print(f'#{tc}', end=' ')
+    print(*code)        
