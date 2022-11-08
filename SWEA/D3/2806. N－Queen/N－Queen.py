c65cdbb47d754bfeb08f1cd20d10139f442c6a56
Dasom Han <50664844+Amyhds@users.noreply.github.com>
2022-11-08 16:27:39 +0900

    [D3] Title: N-Queen, Time: 283 ms, Memory: 64,176 KB -BaekjoonHub

--- SWEA/D3/2806. N－Queen/N－Queen.py
+++ SWEA/D3/2806. N－Queen/N－Queen.py
@@ -1,26 +1,28 @@
-#답보고 함! 공부 다시!
-def promising(candidate, row, col):
-    for i in range(len(candidate)):
-        if candidate[i] == col or abs(candidate[i] - col) == row - i:
-            return False
+#https://sso-feeling.tistory.com/415
+def adjacent(x): # x와 i 가 같으면 행이 같은거 근데 for문을 보면 x와 i가 같을 수가 없다.
+    for i in range(x): #인덱스가 행  row[n]값이 열
+        if row[x] == row[i] or abs(row[x] - row[i]) == x - i: # 열이 같거나 대각선이 같으면 false
+            return False # 대각선이 같은경우는 두 좌표에서 행 - 행 = 열 - 열 이 같으면 두개는 같은 대각선상에 있다.
     return True
-
-def DFS(r, queen):
-    global res
-
-    if r == N:   #n-queen을 만족한 상태
-        res += 1
-        return
-
-    for i in range(N):
-        if promising(queen, r, i):
-            queen.append(i)
-            DFS(r+1, queen)
-            queen.pop()
+ 
+#한줄씩 재귀하며 dfs 실행
+ 
+def dfs(x):
+    global result
+ 
+    if x == N:
+        result += 1
+    else:
+        # 각 행에 퀸 놓기
+        for i in range(N): # i 는 열번호 0부터 N 전까지 옮겨가면서 유망한곳 찾기
+            row[x] = i
+            if adjacent(x): # 행,열,대각선 체크함수 true이면 백트래킹 안하고 계속 진행
+                dfs(x + 1)
 
 T = int(input())
-for test_case in range(1, T+1):
+for tc in range(1, T+1):
     N = int(input())
-    res = 0
-    DFS(0, [])
-    print(f'#{test_case} {res}')
+    row = [0] * N
+    result = 0
+    dfs(0)
+    print(f"#{tc} {result}")
