a00903ff88ef3c509307f4f8eae61de67639414b
Dasom Han <50664844+Amyhds@users.noreply.github.com>
2022-11-14 09:18:36 +0900

    [D3] Title: 진용이네 주차타워, Time: 324 ms, Memory: 66,572 KB -BaekjoonHub

--- SWEA/D3/9280. 진용이네 주차타워/진용이네 주차타워.py
+++ SWEA/D3/9280. 진용이네 주차타워/진용이네 주차타워.py
@@ -1,37 +1,30 @@
 from collections import deque
+
 T = int(input())
-for test_case in range(1, T+1):
+for tc in range(1, T+1):
     n, m = map(int, input().split())
-    R = []
-    W = [0]
-    lot = [0]*n
+    R = [int(input()) for _ in range(n)]
+    W = [int(input()) for _ in range(m)]
+    park = [0] * n
     wait = deque()
-    res = 0
-    for i in range(n):
-        R.append(int(input()))
-    for i in range(m):
-        W.append(int(input()))   
+    fee = 0
     for _ in range(m*2):
         car = int(input())
         if car > 0:
             for i in range(n):
-                if lot[i] == 0:
-                    lot[i] = car
-                    res += R[i]*W[car]
+                if park[i] == 0:
+                    park[i] = car
+                    fee += R[i] * W[car-1]
                     break
             else:
-                wait.append(car)                
-        else:
-            for i in range(n):
-                if lot[i] == -1*car:
-                    lot[i] = 0
-                    if wait:
-                        wait_car = wait.popleft()
-                        lot[i] = wait_car
-                        res += R[i]*W[wait_car]
-                    break
-    print(f'#{test_case} {res}')
+                wait.append(car)
+        if car < 0:
+            loc = park.index(abs(car))
+            park[loc] = 0
+            if wait:
+                new_car = wait.popleft()
+                park[loc] = new_car
+                fee += R[loc] * W[new_car - 1]
+    print(f"#{tc} {fee}")
+                 
                 
-                
-                    
-            
