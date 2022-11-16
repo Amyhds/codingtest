3a343b447fb6e7e6c2db6ebb0673babf06a17a80
Dasom Han <50664844+Amyhds@users.noreply.github.com>
2022-11-16 22:01:55 +0900

    [D3] Title: 성공적인 공연 기획, Time: 155 ms, Memory: 61,708 KB -BaekjoonHub

--- SWEA/D3/4789. 성공적인 공연 기획/성공적인 공연 기획.py
+++ SWEA/D3/4789. 성공적인 공연 기획/성공적인 공연 기획.py
@@ -1,13 +1,11 @@
 T = int(input())
 for tc in range(1, T+1):
-    clap = input()
+    clap_ppl = input()
+    ppl = int(clap_ppl[0])
     hire = 0
-    ppl = 0
-    for i in range(len(clap)):          
-        if int(clap[i]) != 0:
-            if ppl >= i:
-                ppl += int(clap[i])
-            else:
-                hire += i - ppl
-                ppl = i + int(clap[i])
-    print(f'#{tc} {hire}')
+    for i in range(1, len(clap_ppl)):
+        if ppl < i:
+            hire += i - ppl
+            ppl += i - ppl
+        ppl += int(clap_ppl[i])
+    print(f"#{tc} {hire}")
