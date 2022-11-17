1bc246783a1afc880e4ac86860df7d9552417efd
Dasom Han <50664844+Amyhds@users.noreply.github.com>
2022-11-17 22:52:30 +0900

    [D3] Title: 최장 경로, Time: 163 ms, Memory: 61,744 KB -BaekjoonHub

--- SWEA/D3/2814. 최장 경로/최장 경로.py
+++ SWEA/D3/2814. 최장 경로/최장 경로.py
@@ -1,26 +1,25 @@
-#흐음 잘 이해가 안간다
-def DFS(i, cnt):
+def dfs(node, cnt):
     global max_L
     if max_L < cnt:
             max_L = cnt
-    for j in range(1, N+1):
-        if not visited[j] and graph[i][j]:
-            visited[j] = 1 
-            DFS(j, cnt + 1)
-            visited[j] = 0
+    for next in graph[node]:
+        if not visited[next]:
+            visited[next] = 1
+            dfs(next, cnt + 1)
+            visited[next] = 0
 
 T = int(input())
 for test_case in range(1, T+1):
     N, M = map(int, input().split())
-    graph = [[0]*(N+1) for _ in range(N+1)]
+    graph = [[]*(N+1) for _ in range(N+1)]
     visited = [0] * (N+1)
     max_L = 0
     for i in range(M):
         a, b = map(int, input().split())
-        graph[a][b] = 1
-        graph[b][a] = 1
-    for i in range(1, N+1):
-        visited[i] = 1
-        DFS(i, 1)
-        visited[i] = 0
+        graph[a].append(b)
+        graph[b].append(a)
+    for node in range(1, N+1):
+        visited[node] = 1
+        dfs(node, 1)
+        visited[node] = 0
     print(f'#{test_case} {max_L}')
