338097e35f770d216262360c1f55ce27ab9429b9
Dasom Han <50664844+Amyhds@users.noreply.github.com>
2022-11-17 11:18:24 +0900

    [D3] Title: 삼성시의 버스 노선, Time: 210 ms, Memory: 64,360 KB -BaekjoonHub

--- SWEA/D3/6485. 삼성시의 버스 노선/삼성시의 버스 노선.py
+++ SWEA/D3/6485. 삼성시의 버스 노선/삼성시의 버스 노선.py
@@ -1,18 +1,15 @@
 T = int(input())
-for test_case in range(1, T+1):
+for tc in range(1, T+1):
     N = int(input())
-    bus = {}
-    for i in range(1,5001):
-        bus[i] = 0
+    bus_stop= [0] * 5001
     for i in range(N):
         A, B = map(int, input().split())
         for j in range(A, B+1):
-            bus[j] += 1
+            bus_stop[j] += 1
     P = int(input())
-    stop = []
+    res = [0] * P
+    target_stop = [int(input()) for _ in range(P)]
     for i in range(P):
-        stop.append(int(input()))
-    print('#'+str(test_case), end = ' ')
-    for j in range(P):
-        print(bus[stop[j]], end = ' ')
-    print()
+        res[i] = bus_stop[target_stop[i]]
+    print(f"#{tc}", end = " ")
+    print(*res)
