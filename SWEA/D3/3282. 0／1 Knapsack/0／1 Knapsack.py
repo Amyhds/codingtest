d81af0dd8e2110cd1395457ff4bda5331b8ef1f1
Dasom Han <50664844+Amyhds@users.noreply.github.com>
2022-11-17 22:05:14 +0900

    [D3] Title: 0/1 Knapsack, Time: 205 ms, Memory: 63,564 KB -BaekjoonHub

--- SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.py
+++ SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.py
@@ -1,22 +1,20 @@
-def knapsack(K, N, V, C):
+def knapsack(volume, cost):
     bag = [[0 for _ in range(K + 1)] for _ in range(N + 1)]
-    for i in range(N + 1):
-        for v in range(K + 1):
-            if i == 0 or v == 0:
-                bag[i][v] = 0
-            elif V[i - 1] <= v:
-                bag[i][v] = max(bag[i - 1][v], C[i - 1] + bag[i - 1][v - V[i - 1]])
+    for i in range(1, N + 1):
+        for j in range(1, K + 1):
+            if volume[i - 1] <= j:
+                bag[i][j] = max(bag[i - 1][j], cost[i - 1] + bag[i - 1][j - volume[i - 1]])
             else:
-                bag[i][v] = bag[i - 1][v]
+                bag[i][j] = bag[i - 1][j]
     return bag[N][K]
 
 T = int(input())
 for tc in range(1, T + 1):
     N, K = map(int, input().split())
-    V = []
-    C = []
+    volume = []
+    cost = []
     for _ in range(N):
         v, c = map(int, input().split())
-        V.append(v)
-        C.append(c)
-    print(f'#{tc} {knapsack(K, N, V, C)}')
+        volume.append(v)
+        cost.append(c)
+    print(f'#{tc} {knapsack(volume, cost)}')
