c6508ee86ea55c98da1309688620de998d335250
Dasom Han <50664844+Amyhds@users.noreply.github.com>
2022-11-17 12:19:09 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 506 ms, Memory: 63,340 KB -BaekjoonHub

--- SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
@@ -1,19 +1,27 @@
-# 답보고 함 다시 복습하자
-for tc in range(1,int(input())+1):
-    data, K = input().split() 
-    K = int(K)
-    N = len(data)
-    now = set([data])
-    nxt = set()
-    for _ in range(K):
-        while now:
-            s = now.pop()
-            s = list(s)
-            for i in range(N):
-                for j in range(i+1,N):
-                    s[i],s[j] = s[j],s[i]
-                    nxt.add(''.join(s))
-                    s[i], s[j] = s[j], s[i]
-        now,nxt = nxt,now
-
-    print('#{} {}'.format(tc,max(map(int,now))))
+# 너무 어렵네...https://www.youtube.com/watch?v=vGU8h1rneGw
+def dfs(n):
+    global ans
+    if n == count:
+        ans = max(ans, int("".join(map(str, num_lst))))
+        return
+    for i in range(L-1):
+        for j in range(i+1, L):
+            num_lst[i], num_lst[j] = num_lst[j], num_lst[i]
+            chk = int("".join(map(str, num_lst)))
+            if (n, chk) not in visited:
+                dfs(n+1)
+                visited.append((n, chk))
+            num_lst[i], num_lst[j] = num_lst[j], num_lst[i]
+            
+T = int(input())
+for tc in range(1, T+1):
+    num, count = input().split()
+    count = int(count)
+    num_lst = []
+    for n in num:
+        num_lst.append(int(n))
+    L = len(num_lst)
+    ans = 0
+    visited = []
+    dfs(0)
+    print(f"#{tc} {ans}")
