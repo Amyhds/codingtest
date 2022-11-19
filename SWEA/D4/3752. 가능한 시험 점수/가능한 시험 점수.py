ff979b403b33b112df60202916f93cc5ceaa1e99
Dasom Han <50664844+Amyhds@users.noreply.github.com>
2022-11-19 20:59:55 +0900

    [D4] Title: 가능한 시험 점수, Time: 216 ms, Memory: 62,776 KB -BaekjoonHub

--- SWEA/D4/3752. 가능한 시험 점수/가능한 시험 점수.py
+++ SWEA/D4/3752. 가능한 시험 점수/가능한 시험 점수.py
@@ -7,10 +7,9 @@ for tc in range(1, T+1):
     case[0] = 1
     res = [0]
     for s in score:
-        temp = list(res)
-        for t in temp:
-            if not case[s+t]:
-                case[s+t] = 1
-                res.append(s+t)
+        for i in range(len(res)):
+            if not case[s+res[i]]:
+                case[s+res[i]] = 1
+                res.append(s+res[i])
     print(f'#{tc} {len(res)}')
     
