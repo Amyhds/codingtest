b0a6aa57f2ece926c4c0439f77655546347b28be
Dasom Han <50664844+Amyhds@users.noreply.github.com>
2022-11-20 11:49:24 +0900

    [D3] Title: 숫자 조작, Time: 172 ms, Memory: 61,708 KB -BaekjoonHub

--- SWEA/D3/13428. 숫자 조작/숫자 조작.py
+++ SWEA/D3/13428. 숫자 조작/숫자 조작.py
@@ -1,17 +1,3 @@
-def dfs(cnt):
-    global max_N, min_N
-    if cnt == 1:
-        
-        return
-    for i in range(len(N)-1):
-        for j in range(i+1, len(N)): 
-            N[i], N[j] = N[j], N[i]
-            if N[0] == '0':
-                N[i], N[j] = N[j], N[i]
-                continue
-            dfs(cnt+1)
-            N[i], N[j] = N[j], N[i]
-
 T = int(input())
 for tc in range(1, T+1):
     N = list(input())
