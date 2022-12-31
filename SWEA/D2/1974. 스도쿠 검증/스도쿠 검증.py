71c6de40d44b97da37c0f4aed187f3ed6c44dd34
Dasom Han <50664844+Amyhds@users.noreply.github.com>
2022-12-31 21:50:12 +0900

    [D2] Title: 스도쿠 검증, Time: 143 ms, Memory: 59,360 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
@@ -1,33 +1,76 @@
-T = int(input())
-for tc in range(1, T+1):
-    sudoku = [list(map(int, input().split())) for _ in range(9)]
-    res = 1
-    #hzt
-    for i in range(9):
-        chk = [0] * 9
-        for j in range(9):
-            chk[sudoku[i][j] - 1] = 1
-        if sum(chk) != 9:
-            res = 0
-            break
-    #vtc
-    if res == 1:
-        for i in range(9):
-            chk = [0] * 9
-            for j in range(9):
-                chk[sudoku[j][i] - 1] = 1
-            if sum(chk) != 9:
-                res = 0
-                break
-    #3*3
-    if res == 1:
-        for i in range(0, 9, 3):
-            for j in range(0, 9, 3):
-                chk = [0] * 9
-                for x in range(3):
-                    for y in range(3):
-                        chk[sudoku[i+x][j+y] - 1] = 1
-                if sum(chk) != 9:
-                    res = 0
-                    break
-    print(f'#{tc} {res}')
+import java.util.Scanner;
+import java.io.FileInputStream;
+import java.util.Arrays;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+        int[ ][ ] sudoku = new int[9][9];
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+            int answer = 1;
+            for(int row = 0; row < 9; row++)
+            {
+                for(int col = 0; col < 9; col++)
+                {
+                    sudoku[row][col] = sc.nextInt();
+                }
+            }
+            //row and col
+            for(int row = 0; row < 9; row++)
+            {
+                int[ ] check_row = new int[9]; 
+                int[ ] check_col = new int[9]; 
+                Arrays.fill(check_row, 0);
+                Arrays.fill(check_col, 0);
+                for(int col = 0; col < 9; col++)
+                {
+                    check_row[sudoku[row][col] - 1] = 1;
+                    check_col[sudoku[col][row] - 1] = 1;
+                }
+                for(int i = 0; i < 9; i++)
+                {
+                    if(check_row[i] == 0)
+                    {
+                        answer = 0;
+                        break;
+                    }
+                    if(check_col[i] == 0)
+                    {
+                        answer = 0;
+                        break;
+                    }
+                }
+            }
+            //3*3
+            for(int row = 0; row < 9; row += 3)
+            {
+                for(int col = 0; col < 9; col += 3)
+                {
+                    int[ ] check_square = new int[9];
+                    Arrays.fill(check_square, 0);
+                    for(int i = 0; i < 3; i++) 
+                    {
+                        for(int j = 0; j < 3; j++)
+                        {
+                            check_square[sudoku[row + i][col + j] - 1] = 1;
+                        }
+                    }
+                    for(int k = 0; k < 9; k++)
+                    {
+                        if(check_square[k] == 0)
+                        {
+                            answer = 0;
+                            break;
+                        }
+                    }
+                }
+            }
+            System.out.println("#" + (test_case) + " " + answer);	
+		}
+	}
+}
