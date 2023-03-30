eccba7d76343fd3118e93533dde029fbf351ca3b
Dasom Han <50664844+Amyhds@users.noreply.github.com>
2023-03-30 16:56:22 +0900

    [Silver I] Title: 숨바꼭질, Time: 300 ms, Memory: 55960 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -4,45 +4,48 @@ import java.util.Scanner;
 
 public class Main {
 
-	static int[] location;
 	static int N, K;
-	
-	private static boolean isValid(int loc) {
-		if (loc < 0 || loc > 100000) return false;
-		if (location[loc] != 0) return false;
+	static int[] location;
+
+	public static boolean isValid(int loc) {
+		if (loc < 0 || loc > 100000) // 범위를 벗어나거나
+			return false;
+		if (location[loc] != 0) // 그 위치에 이미 다녀갔거나
+			return false;
 		return true;
 	}
-	
-	private static void find() {
-		
-		Queue<Integer> que = new ArrayDeque<Integer>();
-		que.offer(N);
-		int curr = 0;
-		int time = 0;
-		int size = 0;
+
+	public static void bfs() {
+		Queue<int[]> que = new ArrayDeque<>();
+		que.offer(new int[] { N, 0 }); // 시작(수빈)
+
 		while (!que.isEmpty()) {
-			size = que.size();
-			for (int i = 0; i < size; i++) {
-				curr = que.poll();
-				location[curr] = time;
-				if (curr == K) return;
-				if (isValid(curr - 1))	que.offer(curr - 1);
-				if (isValid(curr + 1))	que.offer(curr + 1);
-				if (isValid(curr * 2))	que.offer(curr * 2);
-			}
+			int[] subin = que.poll();
+			int currLoc = subin[0];
+			int time = subin[1];
+			location[currLoc] = time;
+			if (currLoc == K)
+				return;
+
+			if (isValid(currLoc - 1))
+				que.offer(new int[] { currLoc - 1, time + 1 });
+			if (isValid(currLoc + 1))
+				que.offer(new int[] { currLoc + 1, time + 1 });
+			if (isValid(currLoc * 2))
+				que.offer(new int[] { currLoc * 2, time + 1 });
+
 			time++;
 		}
 
 	}
-	
+
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		
-		N = sc.nextInt();
-		K = sc.nextInt();
-		location = new int[100001];
-		find();
+		N = sc.nextInt(); // 수빈 위치
+		K = sc.nextInt(); // 동생 위치
+		location = new int[100001]; // 수직선
+		bfs();
 		System.out.println(location[K]);
 	}
 
