13cc841d209da156338c0bf800dbbfa4c6c95c45
Dasom Han <50664844+Amyhds@users.noreply.github.com>
2023-04-12 16:41:14 +0900

    [Gold IV] Title: 문자열 폭발, Time: 564 ms, Memory: 62240 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -10,19 +10,22 @@ public class Main {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		String str = bf.readLine();
 		String bomb = bf.readLine();
-		int len = bomb.length();
+		int strlen = str.length();
+        int bomblen = bomb.length();
 		Stack<Character> stack = new Stack<>();
-		for (int i = 0; i < str.length(); i++) {
+		for (int i = 0; i < strlen; i++) {
 			char temp = str.charAt(i);
 			stack.push(temp);
 			boolean isSame = true;
-			if (stack.size() >= len) {
-				for (int j = 0; j < len; j++) {
-					if (stack.get(stack.size() - len + j) != bomb.charAt(j))
-						isSame = false;
+			if (stack.size() >= bomblen) {
+				for (int j = 0; j < bomblen; j++) {
+					if (stack.get(stack.size() - bomblen + j) != bomb.charAt(j)) {
+                        isSame = false;
+                        break;
+                    }
 				}
 				if (isSame) {
-					for (int j = 0; j < len; j++) {
+					for (int j = 0; j < bomblen; j++) {
 						stack.pop();
 					}
 				}
