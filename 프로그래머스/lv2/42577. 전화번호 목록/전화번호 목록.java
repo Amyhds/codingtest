db90213f5aa6dfea88b5adcbeb4da23f145d107e
Dasom Han <50664844+Amyhds@users.noreply.github.com>
2023-07-28 22:06:36 +0900

    [level 2] Title: 전화번호 목록, Time: 341.35 ms, Memory: 205 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,29 +2,29 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-//         boolean answer = true;
+        boolean answer = true;
         
-//         HashMap<String, String> hm = new HashMap<>();
-//         for(String num : phone_book) {
-//             hm.put(num, num);
-//         }
+        HashMap<String, String> hm = new HashMap<>();
+        for(String num : phone_book) {
+            hm.put(num, num);
+        }
         
-//         int N = phone_book.length;
         
-//         for(int i = 0; i < N - 1; i++) {
-//             for(int j = i + 1; j < N; j++) {
-//                 hm.get()
-//             }
-//         }
-        
-//         return answer;
-        
-        Arrays.sort(phone_book);
-        for (int i = 0; i < phone_book.length - 1; i++){        
-            if (phone_book[i + 1].startsWith(phone_book[i])){
-                return false;
+        for(int i = 0; i < phone_book.length; i++) {
+            for(int j = 0; j < phone_book[i].length(); j++) {
+                if(hm.containsKey(phone_book[i].substring(0, j)))
+                   answer = false;
             }
         }
-        return true;
+        
+        return answer;
+        
+        // Arrays.sort(phone_book);
+        // for (int i = 0; i < phone_book.length - 1; i++){        
+        //     if (phone_book[i + 1].startsWith(phone_book[i])){
+        //         return false;
+        //     }
+        // }
+        // return true;
     }
 }
