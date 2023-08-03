2d2590a566a8efec1b808f2ffce4731f8b909b7c
Dasom Han <50664844+Amyhds@users.noreply.github.com>
2023-08-03 22:18:59 +0900

    [Silver III] Title: 바이러스, Time: 68 ms, Memory: 34128 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,22 +1,23 @@
 from collections import deque
 
-def BFS(graph, start, visited):
-    que = deque([start])
-    visited[start] = 1
+def BFS(graph, V):
+    visited = []
+    que = deque([V])
     while que:
-        v = que.popleft()
-        for i in graph[v]:
-            if visited[i] == 0:
-                visited[i] = 1
-                que.append(i)
-    
-N = int(input())
-pair = int(input())
-graph = [[] for _ in range(N+1)]
-for _ in range(pair):
+        n = que.popleft()
+        if n not in visited:
+            visited.append(n)
+            que.extend(graph[n])
+    return visited
+
+C = int(input())
+P = int(input())
+
+graph = {i:[] for i in range(C + 1)}
+
+for i in range(1, P+1):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
-visited = [0] * (N+1)
-BFS(graph, 1, visited)
-print(sum(visited)-1)
+
+print(len(BFS(graph, 1))-1)
