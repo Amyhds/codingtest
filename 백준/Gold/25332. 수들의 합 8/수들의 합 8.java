5ba839b38c90140fe682c96a50489327988d7d3b
Dasom Han <dasomhan524@gmail.com>
2024-01-16 19:20:34 +0900

    [Gold III] Title: 수들의 합 8, Time: 712 ms, Memory: 71296 KB -BaekjoonHub

--- 백준/Gold/25332. 수들의 합 8/수들의 합 8.java
+++ 백준/Gold/25332. 수들의 합 8/수들의 합 8.java
@@ -1,65 +1,40 @@
-import java.io.*;
-import java.util.*;
- 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));        
-        StringBuilder sb = new StringBuilder();      
-        
-        int N = Integer.parseInt(br.readLine());
- 
-        int[] arr1 = new int[N + 1];
-        int[] arr2 = new int[N + 1];
-        long[] arr3 = new long[N + 1];
- 
-        StringTokenizer st = new StringTokenizer(br.readLine());
- 
-        for(int i=1; i<N + 1; i++){
-            arr1[i] = Integer.parseInt(st.nextToken());
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+        int[] A = new int[N + 1];
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        for (int i = 1; i <= N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
         }
- 
-        st = new StringTokenizer(br.readLine());
- 
-        for(int i=1; i<N+1; i++){
-            arr2[i] = Integer.parseInt(st.nextToken());
+        int[] B = new int[N + 1];
+        st = new StringTokenizer(bf.readLine());
+        for (int i = 1; i <= N; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
         }
- 
-        for(int i=1; i<N+1; i++){
-            arr3[i] = arr2[i] - arr1[i];
+        long[] sum = new long[N + 1];
+        for (int i = 1; i <= N; i++) {
+            sum[i] = sum[i - 1] + (A[i] - B[i]);
         }
- 
-        for(int i=1; i<N+1; i++){
-            arr3[i] = arr3[i-1] + arr3[i];
-        }
- 
-        long result = 0;
-        long cnt = 0;
- 
-        HashMap<Long, Long> hm = new HashMap<>();
- 
-        for(int i=1; i<N+1; i++){
-            if(arr3[i] == 0){
-                result++;                
+
+        long answer = 0;
+        HashMap<Long, Long> map = new HashMap<>();
+        for (int i = 1; i <= N; i++) {
+            if (sum[i] == 0) {
+                answer++;
             }
- 
-            if(hm.containsKey(arr3[i] - 0)){
-                cnt = hm.get(arr3[i] - 0);
-            }else{
-                cnt = 0;
-            }
-            
-            result += cnt;
- 
-            long a = 1;
-            
-            if(hm.containsKey(arr3[i])){
-                hm.put(arr3[i], hm.get(arr3[i]) + 1);
-            }else{
-                hm.put(arr3[i], a);
-            }
+            answer += map.getOrDefault(sum[i], 0L);
+            map.put(sum[i], map.getOrDefault(sum[i], 0L) + 1L);
         }
- 
-        System.out.println(result);
-        
+
+        System.out.println(answer);
     }
+
 }
