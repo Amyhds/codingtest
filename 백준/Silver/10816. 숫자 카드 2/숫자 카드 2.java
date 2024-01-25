7a498b21fae9688888d576d43c111acb4e2cbee4
Dasom Han <dasomhan524@gmail.com>
2024-01-25 10:23:21 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1512 ms, Memory: 139004 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N;
+    static int[] cards;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(bf.readLine());
+        cards = new int[N];
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        for (int i = 0; i < N; i++) {
+            cards[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(cards);
+        int M = Integer.parseInt(bf.readLine());
+        st = new StringTokenizer(bf.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            int number = Integer.parseInt(st.nextToken());
+            int result = upperBound(number) - lowerBound(number) + 1;
+            sb.append(result + " ");
+        }
+        System.out.println(sb);
+    }
+
+    public static int lowerBound(int number) {
+        int start = 0;
+        int end = N - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (cards[mid] < number) {
+                start = mid + 1;
+            } else if (cards[mid] >= number) {
+                end = mid - 1;
+            }
+        }
+        return start;
+    }
+
+    public static int upperBound(int number) {
+        int start = 0;
+        int end = N - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (cards[mid] > number) {
+                end = mid - 1;
+            } else if (cards[mid] <= number) {
+                start = mid + 1;
+            }
+        }
+        return end;
+    }
+}
