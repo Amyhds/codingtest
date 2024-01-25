aefc42cbe5a124956b1d9f86eeb8c8d440dcbc83
Dasom Han <dasomhan524@gmail.com>
2024-01-25 13:23:48 +0900

    [Silver II] Title: 나무 자르기, Time: 1036 ms, Memory: 121424 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, M, max, answer;
+    static int[] trees;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        trees = new int[N];
+        st = new StringTokenizer(bf.readLine());
+        for (int i = 0; i < N; i++) {
+            trees[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(trees);
+        max = trees[N - 1];
+        answer = 0;
+        int start = 0;
+        int end = max;
+        int mid = 0;
+        while (start <= end) {
+            mid = (start + end) / 2;
+            long result = cut(mid);
+            if (result < M) {
+                end = mid - 1;
+            } else {
+                answer = mid;
+                start = mid + 1;
+            }
+        }
+        System.out.println(answer);
+    }
+
+    public static long cut(int cutter) {
+        long height = 0;
+        for (int i = 0; i < N; i++) {
+            int remain = trees[i] - cutter;
+            if (remain > 0) {
+                height += remain;
+            }
+        }
+        return height;
+    }
+
+}
