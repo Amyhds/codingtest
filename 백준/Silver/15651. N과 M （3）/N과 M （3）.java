ec221f8bb381848b0f1db5ddfef313121540de19
Dasom Han <dasomhan524@gmail.com>
2024-01-31 21:29:21 +0900

    [Silver III] Title: N과 M (3), Time: 696 ms, Memory: 212300 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+
+	static int N, M;
+	static int[] arr;
+	static StringBuilder sb;
+
+	public static void comb(int cnt) {
+		if (cnt == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(arr[i] + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+
+		for (int i = 1; i <= N; i++) {
+			arr[cnt] = i;
+			comb(cnt + 1);
+		}
+	}
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		sb = new StringBuilder();
+		N = sc.nextInt();
+		M = sc.nextInt();
+		arr = new int[M];
+		comb(0);
+		System.out.println(sb.toString());
+	}
+
+}
