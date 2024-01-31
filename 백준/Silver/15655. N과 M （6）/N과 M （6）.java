d15c7e88887af3d983aa205cdba24ecae0121a00
Dasom Han <dasomhan524@gmail.com>
2024-01-31 22:40:56 +0900

    [Silver III] Title: N과 M (6), Time: 144 ms, Memory: 15852 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, M;
+    static int[] array;
+    static int[] selected;
+    static boolean[] visited;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        sb = new StringBuilder();
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        array = new int[N];
+//        visited = new boolean[N];
+        selected = new int[M];
+        st = new StringTokenizer(bf.readLine());
+        for (int i = 0; i < N; i++) {
+            array[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(array);
+        permutation(0, 0);
+        System.out.println(sb);
+    }
+
+    public static void permutation(int cnt, int start) {
+        if (cnt == M) {
+            for (int num : selected
+            ) {
+                sb.append(num + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = start; i < N; i++) {
+//            if (visited[i]) {
+//                continue;
+//            }
+//            visited[i] = true;
+            selected[cnt] = array[i];
+            permutation(cnt + 1, i + 1);
+//            visited[i] = false;
+        }
+    }
+
+}
