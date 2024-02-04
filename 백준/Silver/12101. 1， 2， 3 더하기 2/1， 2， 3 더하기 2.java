82d4ae1d00a7b993f5d655c30fc57de655a243b7
Dasom Han <dasomhan524@gmail.com>
2024-02-04 18:46:28 +0900

    [Silver I] Title: 1, 2, 3 더하기 2, Time: 140 ms, Memory: 15832 KB -BaekjoonHub

--- 백준/Silver/12101. 1， 2， 3 더하기 2/1， 2， 3 더하기 2.java
+++ 백준/Silver/12101. 1， 2， 3 더하기 2/1， 2， 3 더하기 2.java
@@ -1,53 +1,50 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int n;
+    static int n, count;
     static long k;
-    static List<String> expressions;
+    static int[] arr;
+    static boolean isValid;
+    static StringBuilder sb;
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
+        sb = new StringBuilder();
         n = Integer.parseInt(st.nextToken());
         k = Long.parseLong(st.nextToken());
-        expressions = new ArrayList<>();
-        for (int i = 1; i <= n; i++) {
-            int[] selected = new int[i];
-            permutation(i, 0, selected);
-        }
-        Collections.sort(expressions);
-        if (expressions.size() < k) {
-            System.out.println(-1);
+        count = 0;
+        arr = new int[11];
+        func(0, 0);
+        if (isValid) {
+            System.out.println(sb.toString());
         } else {
-            System.out.println(expressions.get((int) (k - 1)));
+            System.out.println(-1);
         }
     }
 
-    public static void permutation(int pick, int cnt, int[] selected) {
-        if (cnt == pick) {
-            int sum = 0;
-            String expression = "";
-            for (int i = 0; i < pick - 1; i++) {
-                sum += selected[i];
-                expression += selected[i] + "+";
-            }
-            sum += selected[pick - 1];
-            expression += selected[pick - 1];
-            if (sum == n) {
-                expressions.add(expression);
+    public static void func(int sum, int length) {
+        if (sum > n) {
+            return;
+        }
+        if (sum == n) {
+            count++;
+            if (count == k) {
+                isValid = true;
+                for (int i = 0; i < length - 1; i++) {
+                    sb.append(arr[i] + "+");
+                }
+                sb.append(arr[length - 1]);
             }
             return;
         }
-        for (int i = 1; i <= 3; i++) {
-            selected[cnt] = i;
-            permutation(pick, cnt + 1, selected);
+        for (int num = 1; num <= 3; num++) {
+            arr[length] = num;
+            func(sum + num, length + 1);
         }
     }
 
