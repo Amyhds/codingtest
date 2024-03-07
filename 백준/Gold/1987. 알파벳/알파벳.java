f1fcaf7d9aba3ac6d5924342fd8f8da92151dcfa
Dasom Han <dasomhan524@gmail.com>
2024-03-07 22:34:49 +0900

    [Gold IV] Title: 알파벳, Time: 864 ms, Memory: 15684 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,55 +1,45 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static int R, C, max;
-	static char[][] board;
-	static int[][] dir = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
-	static Stack<Character> stack;
-	static Set<Character> set;
+    static int R, C, max;
+    static int[][] board;
+    static int[][] dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    static boolean[] visited;
 
-	private static void dfs(int x, int y) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        board = new int[R][C];
+        for (int r = 0; r < R; r++) {
+            String temp = bf.readLine();
+            for (int c = 0; c < C; c++) {
+                board[r][c] = (int) temp.charAt(c) - 'A';
+            }
+        }
+        visited = new boolean[26];
+        dfs(0, 0, 1);
+        System.out.println(max);
+    }
 
-		set.add(board[x][y]);
-		max = Math.max(max, set.size());
-
-		for (int i = 0; i < 4; i++) {
-			int nx = x + dir[i][0];
-			int ny = y + dir[i][1];
-			if (nx < 0 || nx >= R || ny < 0 || ny >= C)
-				continue;
-			if (set.contains(board[nx][ny]))
-				continue;
-			dfs(nx, ny);
-			set.remove(board[nx][ny]);
-		}
-	}
-
-	public static void main(String[] args) throws IOException {
-
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(bf.readLine());
-		R = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
-		board = new char[R][C];
-		stack = new Stack<Character>();
-		set = new HashSet<Character>();
-		for (int r = 0; r < R; r++) {
-			String temp = bf.readLine();
-			for (int c = 0; c < C; c++) {
-				board[r][c] = temp.charAt(c);
-			}
-		}
-
-		max = 1;
-		dfs(0, 0);
-		System.out.println(max);
-	}
-
+    public static void dfs(int r, int c, int length) {
+        max = Math.max(max, length);
+        visited[board[r][c]] = true;
+        for (int d = 0; d < 4; d++) {
+            int nr = r + dir[d][0];
+            int nc = c + dir[d][1];
+            if (nr < 0 || nr >= R || nc < 0 || nc >= C) {
+                continue;
+            }
+            if (!visited[board[nr][nc]]) {
+                dfs(nr, nc, length + 1);
+                visited[board[nr][nc]] = false;
+            }
+        }
+    }
 }
