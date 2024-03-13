59861839eb67c6be006fdec8fd820afee093042a
Dasom Han <dasomhan524@gmail.com>
2024-03-13 11:48:02 +0900

    [Silver I] Title: 숨바꼭질, Time: 196 ms, Memory: 20772 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,52 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	static int N, K;
-	static int[] location;
+    static int N, K;
+    static int[] line;
 
-	public static boolean isValid(int loc) {
-		if (loc < 0 || loc > 100000) // 범위를 벗어나거나
-			return false;
-		if (location[loc] != 0) // 그 위치에 이미 다녀갔거나
-			return false;
-		return true;
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        line = new int[100001];
+        for (int i = 0; i <= 100000; i++) {
+            line[i] = -1;
+        }
+        bfs();
+        System.out.println(line[K]);
+    }
 
-	public static void bfs() {
-		Queue<int[]> que = new ArrayDeque<>();
-		que.offer(new int[] { N, 0 }); // 시작(수빈)
+    public static void bfs() {
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.offer(N);
+        line[N] = 0;
+        while (!queue.isEmpty()) {
+            int curr = queue.poll();
+            int[] next = teleport(curr);
+            for (int i = 0; i < 3; i++) {
+                if (next[i] < 0 || next[i] >= 100001) {
+                    continue;
+                }
+                if (line[next[i]] >= 0) {
+                    continue;
+                }
+                if (next[i] == K) {
+                    line[next[i]] = line[curr] + 1;
+                    return;
+                }
+                line[next[i]] = line[curr] + 1;
+                queue.offer(next[i]);
+            }
+        }
+    }
 
-		while (!que.isEmpty()) {
-			int[] subin = que.poll();
-			int currLoc = subin[0];
-			int time = subin[1];
-			location[currLoc] = time;
-			if (currLoc == K)
-				return;
-
-			if (isValid(currLoc - 1))
-				que.offer(new int[] { currLoc - 1, time + 1 });
-			if (isValid(currLoc + 1))
-				que.offer(new int[] { currLoc + 1, time + 1 });
-			if (isValid(currLoc * 2))
-				que.offer(new int[] { currLoc * 2, time + 1 });
-
-			time++;
-		}
-
-	}
-
-	public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt(); // 수빈 위치
-		K = sc.nextInt(); // 동생 위치
-		location = new int[100001]; // 수직선
-		bfs();
-		System.out.println(location[K]);
-	}
+    public static int[] teleport(int curr) {
+        return new int[]{curr - 1, curr + 1, curr * 2};
+    }
 
 }
